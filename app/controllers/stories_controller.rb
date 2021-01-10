d97d069d351313e5e64c9b7f27929da4076b3bef
benny <benny08118@gmail.com>
2021-01-10 23:48:18 +0800

    story edit fun id done

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,9 +1,24 @@
 class StoriesController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_story,only:[:edit,:update,:destroy]
+  def  index
+    @stories=current_user.stories.order(created_at: :desc)
+  end
   def  new
     @story=current_user.stories.new
   end
+ 
+  def edit
+    
+  end
 
+  def update
+    if @story.update(story_permit)
+      redirect_to stories_path,notice: '文章更新成功'
+    else
+      render :edit
+    end
+  end
   def create
     @story=current_user.stories.new(story_permit)
     if @story.save
@@ -15,6 +30,9 @@ class StoriesController < ApplicationController
   end
 
   private
+  def find_story
+    @story=current_user.stories.find(params[:id])
+  end
   def story_permit
     params.require(:story).permit(:title,:content)
   end
