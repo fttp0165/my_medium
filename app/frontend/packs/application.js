64b493ca25a3067a043c861af3f887d52c58473d
benny <benny08118@gmail.com>
2021-01-10 17:20:22 +0800

    Font Awesome

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,7 +1,3 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
 
 require("@rails/ujs").start()
 require("turbolinks").start()
@@ -17,3 +13,4 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 
 import "controllers"
+import "styles/frontend/index.js"
