2ec8b4dd9e6d4f332622dfb6110e6e328ab91192
benny <benny08118@gmail.com>
2021-02-19 17:26:18 +0800

    modify register

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,12 +42,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
   # If you have extra params to permit, append them to the sanitizer.
    def configure_sign_up_params
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:username],:username)
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
    end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username,:intro,:avatar],:username)
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username,:intro,:avatar])
   end
 
 
