eaaa6d8bffa4045d5439f6c76ae03d0002c2aa67
benny <benny08118@gmail.com>
2021-02-19 12:23:25 +0800

    modify home page difference block show articles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,11 @@ class PagesController < ApplicationController
   before_action :find_story,only: [:show]
 
   def index
-  	@stories=Story.published_stories
+  	@stories=Story.published_stories.limit(4)
+    @stories_clap=Story.published_stories.order(:clap).limit(5)
+    @stories_sample=Story.published_stories.where("clap>5" ).sample(3)
+    @story_sample=Story.published_stories.where("clap>20" ).sample
+    @story_current=Story.published_stories.order(:created_at).first
   end
   def new
     
